8b2ead0c8e108d1e1f90014bfe7a52892b375986
farchan <mfarchan32@gmail.com>
2023-06-04 18:22:27 +0800

    project selesai

--- app/Http/Controllers/KontakController.php
+++ app/Http/Controllers/KontakController.php
@@ -4,25 +4,21 @@
 
 	use App\Models\Kontak;
 	use Illuminate\Http\Request;
+	use Validator;
 
 	class KontakController extends Controller
 	{
-		protected KontakValidate $kontakValidate;
-
-		/**
-		 * @param KontakValidate $kontakValidate
-		 */
-		public function __construct(KontakValidate $kontakValidate)
+		protected $kontakValidate;
+		public function __construct()
 		{
-			$this->kontakValidate = $kontakValidate;
+			$this->kontakValidate = new KontakValidate();
 		}
 
 
 		public function index()
 		{
-			$data = Kontak::orderBy('id', 'desc')->paginate(5)->get();
+			$data = Kontak::orderBy('id', 'desc')->get();
 			return $this->kontakValidate->getShowRespon($data);
-
 		}
 
 		public function store(Request $request)
@@ -31,13 +27,39 @@
 
 		public function show($id)
 		{
+			$data = Kontak::find($id);
+			if ($data){
+				return $this->kontakValidate->getResponse($data,"data $data->nama ditemukan");
+			}else {
+				return $this->kontakValidate->getResponse(false,"data dengan id $id tidak ditemukan");
+			}
 		}
 
 		public function update(Request $request, $id)
 		{
+			$data = Kontak::find($id);
+			$nama = $data->nama;
+			$isValid = Validator::make($request->all(), [
+				'nama' => 'required',
+				'alamat' => 'required',
+				'telepon' => 'required'
+			]);
+			if ($isValid->fails()){
+				return $this->kontakValidate->getResponse($data,'data inputtan kosong');
+			}else {
+				$data->update($request->all());
+				return $this->kontakValidate->getResponse($data,"Data $nama berhasil di ubah menjadi $request->nama");
+			}
 		}
 
 		public function destroy($id)
 		{
+			$data = Kontak::find($id);
+			if ($data){
+				$data->delete();
+				return $this->kontakValidate->getResponse($data,"Data $data->nama berhasil di hapus");
+			}else {
+				return $this->kontakValidate->getResponse(false,"Data dengan id $id tidak ditemukan");
+			}
 		}
 	}
--- app/Http/Controllers/KontakValidate.php
+++ app/Http/Controllers/KontakValidate.php
@@ -4,27 +4,31 @@
 
 	class KontakValidate
 	{
-		public function getShowRespon($data){
+		public function getShowRespon($data)
+		{
 			if ($data->count() > 0) {
 				return response()->json([
 					'status' => true,
-					'message' => 'Success',
+					'message' => sizeof($data)." data ditemukan",
 					'data' => $data
 				], 200);
 			} else {
 				return response()->json([
 					'status' => false,
 					'message' => 'Data masih kosong'
-				],404);
+				], 404);
 			}
 		}
 
-		public function getResponse($data,string $text){
-			if ($data){
-				return response()->json([
-					'status' => true,
-					'message' => $text
-				], $data == true?200:404);
-			}
+		public function getResponse($data, string $text)
+		{
+			if ($data) $isExists = true;
+			else $isExists = false;
+
+			return response()->json(array(
+				'status' => $isExists,
+				'message' => $text
+			), $isExists ? 200 : 404);
+
 		}
 	}
