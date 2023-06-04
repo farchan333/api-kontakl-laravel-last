9615742d9360037576481fe6e343a32d5ed52420
farchan <mfarchan32@gmail.com>
2023-06-04 17:28:21 +0800

    selesai menambahkan KontakSeeder

--- database/seeders/KontakSeeder.php
+++ database/seeders/KontakSeeder.php
@@ -2,12 +2,21 @@
 
 	namespace Database\Seeders;
 
+	use App\Models\Kontak;
+	use Faker\Factory;
 	use Illuminate\Database\Seeder;
 
 	class KontakSeeder extends Seeder
 	{
 		public function run()
 		{
-
+			$faker = Factory::create('id_ID');
+			for($i = 0; $i < 10 ; $i++){
+			    Kontak::create([
+			        'nama' => $faker->name,
+			        'alamat' => $faker->address,
+			        'telepon' => $faker->phoneNumber
+			    ]);
+			}
 		}
 	}
